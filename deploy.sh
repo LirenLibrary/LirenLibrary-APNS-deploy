13d9417b5bd3e51b8aa106995c973bd17ebe96a6
Metaphor <zhiheng.li.metaphor@gmail.com>
2013-01-24 18:47:08 +0800

    refactor code

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,19 @@ TMPDIR="$PRGDIR/temp"
 REMOTE_DIR="~/.liren"
 
 REPOSITORY_SSH=$1
-REPOSITORY_DIR=$2
+REPOSITORY_FILE=$2
+LOCAL_FILE="$TMPDIR/liren-apns.p12"
 REMOTE_SSH=$3
 REMOTE_DIR=$4
 
 echo "preparing local temp directory..."
 mkdir -p "$TMPDIR"
-rm -rf "$TMPDIR" 
+rm -rf "$TMPDIR/*" 
 
 echo "fetching certificates from remote repository..."
-scp -q "$REPOSITORY_SSH:$REPOSITORY_DIR" "$TMPDIR" 
+scp -q "$REPOSITORY_SSH:$REPOSITORY_FILE" "$LOCAL_FILE" 
 
 echo "deploying certificates to remote environment..."
 ssh -q "$REMOTE_SSH" "mkdir -p $REMOTE_DIR"
-scp -q "$TMPDIR/*" "$REMOTE_SSH:$REMOTE_DIR"
+scp -q "$LOCAL_FILE" "$REMOTE_SSH:$REMOTE_DIR"
 
